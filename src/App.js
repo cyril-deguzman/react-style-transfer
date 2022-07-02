178115dd1a95e8d62547c4779d800c481b5ae9c9
cyril-deguzman <cyrilethan.deguzman@gmail.com>
2022-07-02 23:10:01 +0800

    progress bar, message, backtotop added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import antd component library
-import { Col, Row, Layout, Menu, Button, Space, Typography, PageHeader } from 'antd';
+import { Col, Row, Layout, Button, Space, PageHeader, Progress, BackTop, message } from 'antd';
 import { GithubOutlined } from '@ant-design/icons';
 import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
 import { useRef, useState, useEffect } from 'react';
@@ -16,15 +16,17 @@ import logo from './img/logo1.png';
 
 // constants
 const { Header, Content, Footer } = Layout;
-const { Title } = Typography;
 
-const items1 = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
+const success = () => {
+  message.success('Application fully loaded!');
+};
+
+const warning = () => {
+  message.warning('Application is downloading (88mb)!');
+};
 
 const getRatio = (image) => {
-  const maxSize = 256;
+  const maxSize = 220;
   const width = image.shape[0];
   const height = image.shape[1];
 
@@ -59,19 +61,32 @@ const preprocess = (imgData) => {
 // App Component
 const App = () => {
   const [model, setModel] = useState({});
+  const [progress, setProgress]= useState(0.00);
+  const [disabled, setDisabled] = useState(true);
   const canvasRef = useRef()
   const contentRef = useRef()
   const styleRef = useRef()
   
   useEffect(() => {
+    warning()
     fetchModel()
   }, []);
 
   const fetchModel = async () => {
     let fetchedModel = await tf.loadGraphModel('model/model.json', 
-      {onProgress: (value)=>{
-        console.log(value)
-      }})
+    {onProgress: (progress)=>{
+      const progressed = (progress*100).toFixed(2)
+      
+      console.log(`download progress: ${progressed}%`)
+    
+      if(progressed >= 100) {
+        setDisabled(false)
+        console.log('finished')
+        success()
+      }
+      
+      setProgress(progressed)
+    }})
     
     setModel(fetchedModel)
   }
@@ -93,85 +108,95 @@ const App = () => {
 
   return (
     <>
+    <BackTop/>
     <Layout>
-    <ParallaxProvider>
-        <ParallaxBanner 
-          layers={[{ image: bg, speed: -50}]}
-          style={{ }}>
-
-        <Content>
-            <Header className="header" id="navbar">
-              <PageHeader
+        <div id='parallax-ina' style={{
+            background: `url(${bg})`,
+            backgroundSize: 'cover',
+            backgroundAttachment: 'fixed',
+            backgroundPosition: 'center',
+            zIndex: 1
+          }}>
+          <Content>
+              <Header className="header" id="navbar">
+                <PageHeader
+                  style={{
+                    padding: '0 0'
+                  }}>
+                  <img src={logo} height={'100px'} width={'110px'}/>
+                </PageHeader>
+              </Header>
+            <div id='header-content'>
+              <Layout
+                id='main-layout-background'
                 style={{
-                  padding: '0 0'
-                }}>
-                <img src={logo} height={'100px'} width={'110px'}/>
-              </PageHeader>
-            </Header>
-          
-
-          <div id='header-content'>
-            <Layout
-              className="site-layout-background"
-              style={{
-                height: '100%',
-                padding: '24px 0',
-              }}
-            >
-              <Content
-                style={{
-                  padding: '0 24px',
-                  minHeight: 0,
+                  height: '100%',
+                  padding: '24px 0',
+                  background: 'none'
                 }}
               >
-              
-              <Row justify='center'>
-                <Col align='middle'>
-                  <p id='main-title'>
-                    Style Transfer
-                  </p>
-                </Col>
-              </Row>
-              
-              <Row justify='space-evenly'>
-                <Col
-                xs='24'
-                sm='12'
-                md='8'
-                align='middle'>
-                  <p className='title-image'>Content Image</p>
-                  <UploadButton type={'content'} innerRef={contentRef}/>
-                </Col>
-                <Col
-                xs='24'
-                sm='12'
-                md='8'
-                align='middle'>
-                  <p className='title-image'>Style Image</p>
-                  <UploadButton type={'style'} innerRef={styleRef}/>
-                </Col>
-                <Col
+                <Content
+                  style={{
+                    padding: '0 24px',
+                    minHeight: 0,
+                  }}
+                >
+          
+                <Row justify='center'>
+                  <Col align='middle'>
+                    <p id='main-title'>
+                      Style Transfer
+                    </p>
+                  </Col>
+                </Row>
+          
+                <Row justify='space-evenly'>
+                  <Col
                   xs='24'
                   sm='12'
-                  md='8' align='middle'>
-                    <p className='title-image'>Stylized Image</p>
-                    <UploadButton type={'stylized'} innerRef={canvasRef}/>
-                  <div>
-                    <Button type='primary' onClick={handleStyle}
-                    style={{
-                      width: 120,
-                      margin: '13px 0 0 0',
+                  md='8'
+                  align='middle'>
+                    <p className='title-image'>Content Image</p>
+                    <UploadButton type={'content'} innerRef={contentRef}/>
+                  </Col>
+                  <Col
+                  xs='24'
+                  sm='12'
+                  md='8'
+                  align='middle'>
+                    <p className='title-image'>Style Image</p>
+                    <UploadButton type={'style'} innerRef={styleRef}/>
+                  </Col>
+                  <Col
+                    xs='24'
+                    sm='12'
+                    md='8' align='middle'>
+                      <p className='title-image'>Stylized Image</p>
+                      <UploadButton type={'stylized'} innerRef={canvasRef}/>
+                    <div>
+                      <Button type='primary' onClick={handleStyle}
+                      style={{
+                        width: 120,
+                        margin: '13px 0 0 0',
+                        backgroundColor: "#491718", borderColor: "#4a161e" 
+                      }} disabled={disabled}
+                      >Style</Button>
+                    </div>
+                  </Col>
+                </Row>
+                </Content>
+                <Progress 
+                    strokeColor={{
+                      from: '#773806',
+                      to: '#626c16',
                     }}
-                    >Style</Button> 
-                  </div>           
-                </Col>
-              </Row>
-              </Content>
-            </Layout>
-          </div>
-          </Content>
-        </ParallaxBanner>
-      </ParallaxProvider>
+                    percent={progress}
+                    status="active"
+                />
+              </Layout>
+            </div>
+            </Content>
+        </div>
       <Layout
         className = 'site-layout-background'
         style={{
@@ -184,7 +209,6 @@ const App = () => {
           padding: '0 50px',}}
         >
           <How/>
-          
         </Content>
       </Layout>
 
